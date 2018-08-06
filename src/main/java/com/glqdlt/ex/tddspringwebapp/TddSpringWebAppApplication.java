975101d5653bf8a06532e38d949caf05676d69a3
전일웅 <jhun@nm-4ones.com>
2018-08-06 15:11:37 +0900

    ini

---
+++ src/main/java/com/glqdlt/ex/tddspringwebapp/TddSpringWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.glqdlt.ex.tddspringwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSpringWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddSpringWebAppApplication.class, args);
+    }
+}
